7b817f8eff91272f6fa87d59c96f713632efa311
ku234 <knowrop1208@gmail.com>
2022-05-07 20:23:06 +0900

    fix update from command

--- src/components/Svg/Icons/BannerPlaceholder.tsx
+++ src/components/Svg/Icons/BannerPlaceholder.tsx
@@ -5,13 +5,11 @@ import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   const theme = useTheme();
-  const primaryColor = theme.isDark ? "#3C3742" : "#e9eaeb";
-  const secondaryColor = theme.isDark ? "#666171" : "#bdc2c4";
-
   return (
-    <img src="https://cloudfront.ghostnft.io/images/round-512.png" width="72" height="72" />
+    <img src="https://cloudfront.ghostnft.io/localnet/ghostnft/banner-sm.png" width="72" height="72" />
   );
 };
 
 export default Icon;
+
 

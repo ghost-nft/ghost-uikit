24b566d6fe8c781a8acdbdfd1ba71610d3023a3b
ghostpow <ghostnftpow@gmail.com>
2022-05-26 09:46:55 +0900

    fix update from command

--- src/components/SubMenuItems/SubMenuItems.tsx
+++ src/components/SubMenuItems/SubMenuItems.tsx
@@ -5,7 +5,12 @@ import IconComponent from "../Svg/IconComponent";
 import StyledSubMenuItems from "./styles";
 import { SubMenuItemsProps } from "./types";
 
-const SubMenuItems: React.FC<SubMenuItemsProps> = ({ items = [], activeItem, isMobileOnly = false, ...props }) => {
+const SubMenuItems: React.FC<SubMenuItemsProps> = ({
+  items = [],
+  activeItem,
+  isMobileOnly = false,
+  ...props
+}) => {
   return (
     <StyledSubMenuItems
       justifyContent={[isMobileOnly ? "flex-end" : "start", null, "center"]}
@@ -13,23 +18,29 @@ const SubMenuItems: React.FC<SubMenuItemsProps> = ({ items = [], activeItem, isM
       pl={["12px", null, "0px"]}
       $isMobileOnly={isMobileOnly}
     >
-      {items.map(
-        ({ label, href, iconName }) =>
-          label && (
-            <Box key={label} mr="20px">
-              <MenuItem href={href} isActive={href === activeItem} variant="subMenu">
-                {iconName && (
-                  <IconComponent
-                    color={href === activeItem ? "secondary" : "textSubtle"}
-                    iconName={iconName}
-                    mr="4px"
-                  />
-                )}
-                {label}
-              </MenuItem>
-            </Box>
-          )
-      )}
+      <Box>
+        {items.map(
+          ({ label, href, iconName }) =>
+            label && (
+              <Box key={label} mr="20px">
+                <MenuItem
+                  href={href}
+                  isActive={href === activeItem}
+                  variant="subMenu"
+                >
+                  {iconName && (
+                    <IconComponent
+                      color={href === activeItem ? "secondary" : "textSubtle"}
+                      iconName={iconName}
+                      mr="4px"
+                    />
+                  )}
+                  {label}
+                </MenuItem>
+              </Box>
+            )
+        )}
+      </Box>
     </StyledSubMenuItems>
   );
 };

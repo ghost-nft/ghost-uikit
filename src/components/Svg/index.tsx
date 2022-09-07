f5e264a009e2e960297a69d91958eb677856b19b
ghostpow <ghostnftpow@gmail.com>
2022-09-08 00:13:38 +0900

    fix update from command

--- src/components/Svg/index.tsx
+++ src/components/Svg/index.tsx
@@ -15,6 +15,7 @@ export { default as BinanceChainIcon } from "./Icons/BinanceChain";
 export { default as BinanceIcon } from "./Icons/Binance";
 export { default as BlockIcon } from "./Icons/Block";
 export { default as BnbUsdtPairTokenIcon } from "./Icons/BnbUsdtPairToken";
+export { default as EthUsdtPairTokenIcon } from "./Icons/EthUsdtPairToken";
 export { default as BscScanIcon } from "./Icons/BscScan";
 export { default as BunnyCardsIcon } from "./Icons/BunnyCards";
 export { default as BunnyPlaceholderIcon } from "./Icons/BunnyPlaceholder";
